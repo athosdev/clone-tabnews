53fbedbead27f5afe69c2f2fa66c5ace81d84c61
athosdev <72237940+athosdev@users.noreply.github.com>
2023-06-08 17:22:32 +0000

    Alteração de mensagem na Home

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
     return (
-        <h1>Boa Noite meu Amor, eu te Amo mais que tudo na vida 😍❗</h1>
+        <h1>Boa Noite meu Amor, eu te Amo mais que tudo na vida</h1>
 
     )
 }
