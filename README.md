28227b56c20e5d0abcaacae76e6af34ab45767ca
athosdev <72237940+athosdev@users.noreply.github.com>
2023-06-23 16:35:51 +0000

    adiciona scripts `lint:check`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de aprendizagem do Curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,4 @@
-function Home(){
-    return (
-        <h1>Oi amor, eu te amo mais que tudo na vida, hoje e sempre.</h1>
-
-    )
+function Home() {
+  return <h1>Oi amor, eu te amo mais que tudo na vida, hoje e sempre.</h1>;
 }
-
 export default Home;
